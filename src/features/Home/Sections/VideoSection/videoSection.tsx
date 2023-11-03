a7abea95249c4bb73b1cee98ab7c2df47d01e1a7
Marcin Izdebski <mizdebski123@gmail.com>
2023-11-03 10:56:28 +0100

    Background video bug on safari removed

--- src/features/Home/Sections/VideoSection/videoSection.tsx
+++ src/features/Home/Sections/VideoSection/videoSection.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import { ButtonLink, Caption, Logo, LogoSpan, TextWrapper, Video, Wrapper } from './styledVideoSection';
+import { ButtonLink, Caption, Logo, LogoSpan, TextWrapper, Wrapper } from './styledVideoSection'; // Remove 'Video' import here
 import bgvideo from '../../../../common/Videos/bg.mp4';
 import { scrollTop } from '../../../../core/scrollTop';
 
 export const VideoSection = () => {
     return (
         <Wrapper>
-            <Video src={bgvideo} autoPlay loop muted playsInline />
+            <video src={bgvideo} autoPlay loop muted playsInline={true} /> 
             <TextWrapper>
                 <Logo>TREK<LogoSpan>Shop</LogoSpan></Logo>
-                <Caption>Join the group of satisfied customers </Caption>
-                <ButtonLink to='/Log-In' onClick={() => scrollTop()} >Join Us</ButtonLink>
+                <Caption>Join the group of satisfied customers</Caption>
+                <ButtonLink to='/Log-In' onClick={() => scrollTop()}>Join Us</ButtonLink>
             </TextWrapper>
         </Wrapper>
     );
 };
-
